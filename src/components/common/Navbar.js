32f6074de5c7d893c55b799b7e8e0699279e986a
Manan Doshi <MANANDOSHI1301@GMAIL.COM>
2023-04-15 03:43:39 +0530

    Updates

---
+++ src/components/common/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='flex flex-row justify-between items-center'>
+        <div className='flex flex-row items-center'>
+          <div>
+            <img src="#" alt="Vimlesh Doshi" srcset="" />
+          </div>
+          <div>
+            <button>Home</button>
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
